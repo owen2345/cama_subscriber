7390db6842e1e10336e70c651f352111440f21a7
tostasqb <tostasqb@gmail.com>
2017-08-30 16:44:19 +0100

    revised to rails 4.2+

--- db/migrate/20160319112345_subscriber_structure.rb
+++ db/migrate/20160319112345_subscriber_structure.rb
@@ -1,4 +1,4 @@
-class SubscriberStructure < ActiveRecord::Migration[5.0]
+class SubscriberStructure < ActiveRecord::Migration[4.2]
   def change
     create_table :plugins_subscriber_groups do |t|
       t.string :name
